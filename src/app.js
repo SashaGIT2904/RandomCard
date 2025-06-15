e757c01705f16bb0e6cde966fee203c22f8ea22d
SashaGIT2904 <sashaotero5@gmail.com>
2025-06-16 01:02:13 +0200

    Primer commit del proyecto RandomCard

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import "bootstrap";
+import "./style.css";
+
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = () => {
+  document.querySelector('.card').classList.add(generateRandomSuit());
+  document.querySelector('.card').innerHTML = `<span class="numero">${generateRandomNumber()}</span>`;
+};
+
+
+let generateRandomNumber = () =>{
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+
+
+
+}
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "heart", "spade", "club"];
+  let index = Math.floor(Math.random() * suit.length);
+  return suit[index];
+};
+
