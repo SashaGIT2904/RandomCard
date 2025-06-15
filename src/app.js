45203515de5d8444fd92c599b2c0dc0a64c7a320
SashaGIT2904 <sashaotero5@gmail.com>
2025-06-16 01:10:25 +0200

    Comentarios

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
-// Importo Bootstrap y mis estilos
 import "bootstrap";
 import "./style.css";
 
-// Importo imágenes (solo para que no las borre el empaquetador)
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// Cuando la página está lista, genero la carta aleatoria
+// Cuando la página está lista, genera la carta aleatoria
 window.onload = () => {
   // Busco la carta y el span del número
   const card = document.querySelector('.card');
   const numberSpan = document.querySelector('.numero');
 
-  // Quito cualquier palo anterior (por si recargo varias veces)
+  // Quito cualquier palo anterior (recargando varias veces)
   card.classList.remove('spade', 'club', 'heart', 'diamond');
 
   // Genero número y palo aleatorio
